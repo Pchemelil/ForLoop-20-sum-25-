5088d492dde90a0dec80ad2db27ba787602e531d
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-16 21:52:54 +0300

    Add files via upload

---
+++ forLoop(20+25).cpp
@@ -0,0 +1,19 @@
+//137339 Peter Chemelil
+//for loop exercise 2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+int number;
+int sum=0;
+	
+	
+	for(number=20; number<=25; number++)
+	sum = sum+number;
+
+	
+	cout<< "Total sum of numbers between 20 and 25 is:\n"<<sum;
+
+
+}
